9e292b4ef44b5a3e8957f68ebe71e244f97f960e
merlin <gabriel.merlin@student-cs.fr>
2020-03-03 18:10:50 +0100

    Correction des include.

--- schurNumberRecursive/schurNumberConstrainedBuild.h
+++ schurNumberRecursive/schurNumberConstrainedBuild.h
@@ -10,8 +10,8 @@
 #define schurNumberConstrainedBuild_h
 
 #include <stdio.h>
-#include "schurNumberIO.h"
-#include "schurNumberPartitionStruc.h"
+#include "../schurNumberGlobal/schurNumberIO.h"
+#include "../schurNumberGlobal/schurNumberPartitionStruc.h"
 
 unsigned long schurNumberConstrainedBuild(schur_number_partition_t *partitionstruc, struct schurNumberIOAction *action, unsigned long nlimit, mp_limb_t **constraint_partition, mp_size_t constraint_size);
 
