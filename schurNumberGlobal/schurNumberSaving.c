5d419e10cd263c3d1a4790b9a4746357a01b288a
merlin <gabriel.merlin@student-cs.fr>
2020-03-25 16:32:19 +0100

    Création d'une structure gérant la sauvegarde dans un fichier temporaire.

---
+++ schurNumberGlobal/schurNumberSaving.c
@@ -0,0 +1,139 @@
+//
+//  schurNumberSaving.c
+//  SchurNumber
+//
+//  Created by rubis on 14/03/2020.
+//  Copyright © 2020 rubis. All rights reserved.
+//
+
+#include "schurNumberSaving.h"
+
+inline unsigned long fast_powl(unsigned long base, unsigned long exp) {
+    /* Renvoie base^exp en utilisant l'exponentiation rapide. */
+    unsigned long cur_exp = exp;
+    unsigned long res = 1;
+    unsigned long mul = base;
+    
+    while (cur_exp) {
+        if (cur_exp & (unsigned long)1) {
+            res *= mul;
+        }
+        mul *= mul;
+        cur_exp >>= 1;
+    }
+    
+    return res;
+}
+
+void schurNumberSaveInit(schur_number_intermediate_save_t *save, unsigned long p, unsigned long part_pool_count, unsigned long n0) {
+    /* Cette fonction crée un fichier temporaire où sauvegarder les partitions intermédiaires générées au cours de l'exécution du programme.
+     Elle renvoie le descripteur de fichier associé. */
+    char *template = "./schur_number_tmp_save_XXXXXX";
+    int fd = mkstemp(template);
+    
+    if (fd == -1) {
+        fprintf(stderr, "Impossible de créer un fichier temporaire pour sauvegarder les résultats\n");
+    } else {
+        fprintf(stderr, "Fichier temporaire pour sauvegarder les résultats créé : %s\n", template);
+    }
+    
+    save->p = p;
+    
+    unsigned long nbest_estimated = 1<<(2 * p - 1);     // borne sur S(p)
+    
+    // Allocation de best_partition
+    mp_size_t limballoc = INTEGER_2_LIMBSIZE(nbest_estimated);
+    save->best_partition = calloc(sizeof(mp_limb_t *), p);
+    for (unsigned long j = 0; j < p; j++) {
+        save->best_partition[j] = calloc(sizeof(mp_limb_t), limballoc);
+    }
+    
+    save->nbest_estimated = nbest_estimated;
+    save->iremainding = part_pool_count;
+    save->estimated_iternum_initial_partition = calloc(sizeof(unsigned long), part_pool_count);
+    
+    for (unsigned long i = 0; i < part_pool_count; i++) {
+        save->estimated_iternum_initial_partition[i] = fast_powl(p, nbest_estimated - n0);
+    }
+    
+    pthread_mutex_init(save->mutex, NULL);
+}
+
+void schurNumberSaveDealloc(schur_number_intermediate_save_t *save) {
+    
+    for (unsigned long j = 0; j < save->p; j++) {
+        free(save->best_partition[j]);
+    }
+    free(save->best_partition);
+    
+    free(save->estimated_iternum_initial_partition);
+    
+    close(save->fd);
+    
+    pthread_mutex_destroy(save->mutex);
+}
+
+unsigned long schurNumberEstimatedRemainingIteration(unsigned long p, mp_limb_t **partition, unsigned long n, unsigned long n0, unsigned long nbound) {
+    /* Cette fonction estime le nombre d'itérations qu'il restera à parcourir sur l'arbre.
+     Cette estimation s'obtient en dénombrant le nombre de combinaison qu'il reste à effectuer pour énumérer les partitions de nbound sachant que nous sommes parvenus à partition. */
+    
+    unsigned long i = nbound;
+    
+    unsigned long num_configuration = 1; // Nombre de configurations possibles en partant de l'étape n pour atteindre nbound
+    while (i > n) {
+        num_configuration *= p;
+        i--;
+    }
+    
+    unsigned long iternum_estimated = 0;
+    
+    while (i > n0) {
+        unsigned long j = 0;
+        while (j < p && !GET_POINT(partition[j], i)) {
+            j++;
+        }
+        iternum_estimated += (p - j) * num_configuration;
+        num_configuration *= p;
+        i--;
+    }
+    
+    return iternum_estimated;
+}
+
+unsigned long schurNumberSaveUpgrade(schur_number_intermediate_save_t *save, unsigned long nbest, mp_limb_t **partition) {
+    /* Met à jour la meilleure partition trouvée si il y a lieu. */
+    
+    pthread_mutex_lock(save->mutex);
+    
+    if (nbest > save->nbest) {
+        unsigned long p = save->p;
+        
+        mp_size_t limbsize = INTEGER_2_LIMBSIZE(nbest);
+        
+        for (unsigned long i = 0; i < p; i++) {
+            mpn_copyd(save->best_partition[i], partition[i], limbsize);
+        }
+        
+        save->nbest = nbest;
+        save->toprint = 1;
+    }
+    
+    pthread_mutex_unlock(save->mutex);
+    
+    return save->nbest;
+}
+
+void schurNumberSaveToFile(schur_number_intermediate_save_t *save) {
+    /* Cette fonction sauvegarde l'état de la recherche dans le fichier fd. */
+    
+    int fd = save->fd;
+    
+    dprintf(fd, "Nombre de partitions initiales restant : %lu\n", save->iremainding);
+    
+    if (save->toprint) {
+        unsigned long nbest = save->nbest;
+        dprintf(fd, "Taille maximale trouvée : %lu\n", nbest);
+        schurNumberPrintPartition(save->nbest, nbest, save->best_partition);
+    }
+    
+}
---
+++ schurNumberGlobal/schurNumberSaving.h
@@ -0,0 +1,43 @@
+//
+//  schurNumberSaving.h
+//  SchurNumber
+//
+//  Created by rubis on 14/03/2020.
+//  Copyright © 2020 rubis. All rights reserved.
+//
+// Ce fichier déclare les fonctions permettant la sauvegarde des résultats intermédiaires dans un fichier temporaire
+
+#ifndef schurNumberSaving_h
+#define schurNumberSaving_h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "schurNumberIO.h"
+#include "schurNumberPartitionStruc.h"
+
+struct schur_number_intermediate_save_struc {
+    unsigned long p;            // Nombre d'ensembles par partition
+    
+    unsigned long nbest;        // Plus grande taille de partition trouvée
+    mp_limb_t **best_partition; // Partition réalisant cet optimum
+    char toprint;               // Booléen spécifiant si cette partition doit être affichées
+    
+    unsigned long nbest_estimated;  // Plus grande taille de partition estimée
+    size_t iremainding;             // Nombre de partitions initiales restant à explorer
+    unsigned long *estimated_iternum_initial_partition; // Tableau contenant le nombre d'itérations estimées pour chaque partition initiale
+    
+    int fd;                     // Descripteur du fichier où effectuer les sauvegardes intermédiaires
+    pthread_mutex_t *mutex;     // Mutex liée à l'écriture dans fd
+};
+
+typedef struct schur_number_intermediate_save_struc schur_number_intermediate_save_t;
+
+void schurNumberSaveInit(schur_number_intermediate_save_t *save, unsigned long p, size_t part_pool_count, unsigned long n0);
+void schurNumberSaveDealloc(schur_number_intermediate_save_t *save);
+
+unsigned long schurNumberSaveUpgrade(schur_number_intermediate_save_t *save, unsigned long nbest, mp_limb_t **partition);
+
+void schurNumberSaveToFile(schur_number_intermediate_save_t *save);
+
+#endif /* schurNumberSaving_h */
